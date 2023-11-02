096f6ea8a8abf44ae1b9ce254f912ec3390b75ce
Victor P. Araújo <victor@fdge.com.br>
2023-11-02 17:53:38 -0300

    update clen dates

--- scripts/clean_dates.py
+++ scripts/clean_dates.py
@@ -1,18 +1,47 @@
 import re
-import os
+import subprocess
 
-# Lista dos arquivos ou padrões de arquivos a limpar
-files_to_clean = ["config1.properties", "config2.properties", "directory/config3.txt"]
+# Expressão regular para encontrar as linhas de data e hora gerais
+general_pattern = re.compile(
+    r"^\s*#\w{3}\s+\w{3}\s+\d{2}\s+\d{2}:\d{2}:\d{2}\s+BRT\s+\d{4}\s*$")
+# Expressões regulares para as linhas específicas do arquivo instance.cfg
+specific_patterns = {
+    'lastLaunchTime': re.compile(r'^lastLaunchTime=\d+$'),
+    'lastTimePlayed': re.compile(r'^lastTimePlayed=\d+$'),
+    'totalTimePlayed': re.compile(r'^totalTimePlayed=\d+$'),
+}
 
-# Expressão regular para encontrar as linhas de data e hora
-pattern = re.compile(r"^\s*#\w{3}\s+\w{3}\s+\d{2}\s+\d{2}:\d{2}:\d{2}\s+BRT\s+\d{4}\s*$")
+# Caminho para o arquivo específico
+specific_file_path = 'instances/FDGE-SMP/instance.cfg'
 
-for file_name in files_to_clean:
-    if os.path.exists(file_name):
-        with open(file_name, 'r+') as file:
-            lines = file.readlines()
-            file.seek(0)
-            file.truncate()
-            for line in lines:
-                if not pattern.match(line):
+# Obtém a lista de arquivos modificados que estão staged
+staged_files = subprocess.check_output(
+    ['git', 'diff', '--name-only', '--cached'], text=True).splitlines()
+
+# Processa cada arquivo
+files_cleaned = False
+for file_name in staged_files:
+    cleaned = False
+    with open(file_name, 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)
+        file.truncate()
+        for line in lines:
+            if file_name.endswith('instance.cfg'):
+                if not any(pattern.match(line) for pattern in specific_patterns.values()):
                     file.write(line)
+                else:
+                    cleaned = True
+            else:
+                if not general_pattern.match(line):
+                    file.write(line)
+                else:
+                    cleaned = True
+        file.truncate()
+    if cleaned:
+        print(f"Limpeza realizada em: {file_name}")
+        files_cleaned = True
+
+# Restage files se eles foram alterados
+if files_cleaned:
+    subprocess.call(['git', 'add'] + staged_files)
