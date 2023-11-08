04d05c9e2fb41a1c4872c0a8fd64a8584f0daada
Victor P. Araújo <victor@fdge.com.br>
2023-11-08 15:44:09 -0300

    launcher 0.0.8

--- launcher/main.py
+++ launcher/main.py
@@ -4,24 +4,85 @@ import os
 import sys
 import subprocess
 from pkg_resources import parse_version
+from portablemc.standard import Context, Version, VersionManifest, TooMuchParentsError, \
+    JarNotFoundError
+from portablemc.download import DownloadList
+from portablemc.fabric import FabricVersion
+from pathlib import Path
+import time
+import zipfile
+import shutil
+import tempfile
+
 
 # Suponha que a versão atual do script seja um número de versão
 # E que existe um endpoint onde você pode obter a última versão
-VERSAO_ATUAL = "0.0.7"
+VERSAO_ATUAL = "0.0.8"
+GAME_VERSION = "1.20.1"
 URL_BASE = "https://smp.fdge.com.br"
 URL_VERIFICACAO = URL_BASE + "/launcher/latest.txt"
 URL_ATUALIZACAO = URL_BASE + "/launcher/dist"
 
+PATH_BASE = "C:/FDGE-SMP"
+PATH_JAVA = PATH_BASE+"/java"
+URL_JAVA = "https://download.oracle.com/java/17/archive/jdk-17.0.9_windows-x64_bin.zip"
+SUBPATH_JAVA = "/jdk-17.0.9/bin/java.exe"
+
 acao_atual = "Carregando..."
+
+
+def download_and_extract_java():
+
+    if not os.path.exists(PATH_JAVA):
+        updateLabel(f"Criando pasta '{PATH_JAVA}'...", 1)
+        os.makedirs(PATH_JAVA)
+        updateLabel(f"Pasta '{PATH_JAVA}' criada com sucesso.", 1)
+    else:
+        updateLabel(f"A pasta '{PATH_JAVA}' já existe.", 1)
+
+    if os.path.exists(PATH_JAVA):
+        return
+
+    # Corrigindo o caminho para obter o diretório temporário
+    java_zip_path = os.path.join(tempfile.gettempdir(), "java.zip")
+
+    updateLabel(f"Baixando Java...", 1)
+
+    # Melhoria: adicionando tratamento de exceção e download mais eficiente
+    try:
+        # Baixa o arquivo
+        with requests.get(URL_JAVA, stream=True) as response:
+            response.raise_for_status()  # Lança um erro se o download falhar
+            with open(java_zip_path, 'wb') as f:
+                shutil.copyfileobj(response.raw, f)
+
+        updateLabel(f"Java baixado. Iniciando extração...", 1)
+
+        # Extrai o arquivo
+        with zipfile.ZipFile(java_zip_path, 'r') as zip_ref:
+            zip_ref.extractall(PATH_JAVA)
+
+        updateLabel(f"Java extraído com sucesso.", 3)
+
+    except requests.HTTPError as e:
+        updateLabel(f"Erro ao baixar o Java: {e}", 3)
+    except zipfile.BadZipFile as e:
+        updateLabel(f"Arquivo zip corrompido: {e}", 3)
+    except Exception as e:
+        updateLabel(f"Erro desconhecido: {e}", 3)
+    finally:
+        # Remove o arquivo zip baixado após a extração se ele existir
+        if os.path.exists(java_zip_path):
+            os.remove(java_zip_path)
 
 
 def verificar_instalar():
     # Verifica se todas as condições para "Instalar" são atendidas
     if (
-        not os.path.exists("C:/FDGE-SMP") or
-        not os.path.exists("C:/FDGE-SMP/.minecraft") or
-        not os.path.exists("C:/FDGE-SMP/java") or
-        not os.path.exists("C:/FDGE-SMP/version.txt")
+        not os.path.exists(PATH_BASE) or
+        not os.path.exists(PATH_BASE + "/.minecraft") or
+        not os.path.exists(PATH_BASE + "/java") or
+        not os.path.exists(PATH_BASE + "/version.txt")
     ):
         return True
     return False
@@ -34,7 +95,7 @@ def verificar_atualizar():
         ultima_versao_repo = resposta.url.split("/")[-1]
 
         if parse_version(ultima_versao_repo) > parse_version(VERSAO_ATUAL):
-            with open("C:/FDGE-SMP/version.txt", "r") as arquivo:
+            with open(PATH_BASE + "/version.txt", "r") as arquivo:
                 versao_instalada = arquivo.read().strip()
             if parse_version(ultima_versao_repo) > parse_version(versao_instalada):
                 return True
@@ -90,6 +151,23 @@ def verificar_atualizacao():
         label_status.config(text=f"Erro ao verificar atualizações: {e}")
 
 
+def updateLabel(text, delay):
+    def set_label_text():
+        label_status.config(text=text)
+        log(text, "INFO")
+
+    # Agendar a mudança de status após 'delay' milissegundos
+    app.after(delay * 1000, set_label_text)
+
+
+def log(text, type="INFO"):
+    # Grava o texto no arquivo de log com o tipo especificado
+    log_path = PATH_BASE + "/log.txt"
+    with open(log_path, "a") as log_file:
+        log_file.write(
+            f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {type} - {text}\n")
+
+
 def atualizar():
     # Implemente a lógica para a ação "Atualizar" aqui
     print("clicou em atualizar")
@@ -98,14 +176,59 @@ def atualizar():
 
 def instalar():
     # Implemente a lógica para a ação "Instalar" aqui
-    print("clicou em instalar")
-    pass
+    # Exemplo de execução de comando para instalar usando o portablemc
+
+    try:
+
+        pontoMinecraft = PATH_BASE + "/.minecraft"
+        updateLabel(f"Verificando pasta '{pontoMinecraft}'...", 1)
+
+        # Verifica se a pasta já existe
+        if not os.path.exists(pontoMinecraft):
+            # Se não existir, cria a pasta
+            os.mkdir(pontoMinecraft)
+            updateLabel(f"Pasta '{pontoMinecraft}' criada com sucesso.", 1)
+
+        else:
+            updateLabel(f"A pasta '{pontoMinecraft}' já existe.", 1)
+
+        context = Context(Path(pontoMinecraft))
+        version = FabricVersion.with_fabric("1.20.1", context=context)
+        updateLabel(f"Instalando...", 1)
+        version.install()
+        updateLabel(f"Instalado com sucesso.", 3)
+
+        updateLabel(f"Verificando java...", 1)
+        download_and_extract_java()
+        updateLabel(f"Java baixado com sucesso.", 3)
+
+    except Exception as e:
+        label_status.config(text=f"Erro durante a instalação: {e}")
+
+    if not os.path.exists(PATH_BASE + "/version.txt"):
+        # crie o arquivo vestion.txt
+        updateLabel(f"Criando arquivo 'version.txt'...", 1)
+        with open(PATH_BASE + "/version.txt", "w") as arquivo:
+            arquivo.write("0.0.0")
+            updateLabel(f"Arquivo 'version.txt' criado com sucesso.", 1)
+    else:
+        updateLabel(f"Arquivo 'version.txt' já existe.", 1)
+
+    global acao_atual
+    acao_atual = "Jogar"
+    botao.config(text=acao_atual)
 
 
 def jogar():
     # Implemente a lógica para a ação "Jogar" aqui
-    print("clicou em jogar")
-    pass
+    # Exemplo de execução de comando para jogar usando o portablemc
+    try:
+        # Substitua 'SEU_COMANDO_DE_JOGAR' pelo comando real que você deseja executar
+        comando_jogar = 'SEU_COMANDO_DE_JOGAR'
+        subprocess.Popen(comando_jogar, shell=True).wait()
+        label_status.config(text="Iniciando o jogo...")
+    except Exception as e:
+        label_status.config(text=f"Erro ao iniciar o jogo: {e}")
 
 
 def executar_acao():
@@ -141,6 +264,14 @@ if __name__ == "__main__":
     # Crie um único botão com ação dinâmica
     botao = tk.Button(app, text=acao_atual, command=executar_acao)
     botao.pack()
+
+    # Verifica se a pasta já existe
+    if not os.path.exists(PATH_BASE):
+        # Se não existir, cria a pasta
+        os.mkdir(PATH_BASE)
+        updateLabel(f"Pasta '{PATH_BASE}' criada com sucesso.", 1)
+    else:
+        updateLabel(f"A pasta '{PATH_BASE}' já existe.", 1)
 
     # Verificação de atualização automática
     app.after(100, verificar_atualizacao)
